6cb2d520f85832c3957ad8fbcccfc8926ea08ded
DevGuadalajara <a21310422@ceti.mx>
2021-09-14 07:46:31 -0500

    Added IE, Firefox and Chrome demo webdriver

--- DemoTestCases/src/DemoFirefox.java
+++ DemoTestCases/src/DemoFirefox.java
@@ -1,12 +1,12 @@
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class demo
+public class DemoFirefox
 {
     public static void main ( String [] args )
     {
-        System.setProperty( "webdriver.chrome.driver", "C:\\WebDrivers\\chromedriver.exe" );
-        WebDriver driver = new ChromeDriver();
+        System.setProperty( "webdriver.gecko.driver", "C:\\WebDrivers\\geckodriver.exe" );
+        WebDriver driver = new FirefoxDriver();
 
         String url = "https://google.com";
         String title;
--- DemoTestCases/src/DemoIE.java
+++ DemoTestCases/src/DemoIE.java
@@ -1,12 +1,12 @@
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 
-public class demo
+public class DemoIE
 {
     public static void main ( String [] args )
     {
-        System.setProperty( "webdriver.chrome.driver", "C:\\WebDrivers\\chromedriver.exe" );
-        WebDriver driver = new ChromeDriver();
+        System.setProperty( "webdriver.ie.driver", "C:\\WebDrivers\\IEDriverServer.exe" );
+        WebDriver driver = new InternetExplorerDriver();
 
         String url = "https://google.com";
         String title;
